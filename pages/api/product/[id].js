d46d972167fe45948c5c0a9baa419ed438af5fa1
Arran Baker <arranbaker.design@gmail.com>
2021-11-30 09:53:29 +0000

    fetch error catches

--- pages/api/product/[id].js
+++ pages/api/product/[id].js
@@ -5,9 +5,11 @@ export default async (req, res) => {
 
     if (product) {
         res.status(200).json(product);
-    } else if (res.status !== 200) {
+    } else if (res.status === 400) {
         res.status(400).json({ message: "product not found" });
     } else if (res.status === 401) {
+        throw new Error(res.status)
+    } else {
         throw new Error(res.status)
     }
 }
