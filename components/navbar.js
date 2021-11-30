c1d94445f871134e6a34f784134a17af0236eb2e
Arran Baker <arranbaker.design@gmail.com>
2021-11-30 09:39:13 +0000

    link

--- components/navbar.js
+++ components/navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
     return (
         <nav>
             <ul>
-                <li><Link><a className='nav-link' href='/'>HOME</a></Link></li>
+                <li><Link href='/'><a className='nav-link'>HOME</a></Link></li>
                 <li><a className='nav-link' href='#ethos-section'>ETHOS</a></li>
                 <li><a className='nav-link' href='#products-section'>PRODUCTS</a></li>
                 <li><a className='nav-link' href='#contact-section'>CONTACT</a></li>
